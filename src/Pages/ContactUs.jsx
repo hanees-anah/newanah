7e922c25b309acab5d2e43d95154724536694271
Ayush Nama <dev2@scientiaindia.com>
2025-05-05 11:13:20 +0530

    p changs

--- src/Pages/ContactUs.jsx
+++ src/Pages/ContactUs.jsx
@@ -46,7 +46,7 @@ function ContactUS() {
     e.preventDefault("formDataformData", formData);
     console.log("formDataformData",formData)
     try {
-      const response = await axios.post("http://localhost:5000/send-email", formData);
+      const response = await axios.post("https://anahmarketing.com/send-email", formData);
       setShowPopup(true);
 
       setFormData({
